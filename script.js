a85d1000ab26441ad1f333167748921571795b72
Dean_mac_air <dean.f.whitten@gmail.com>
2020-07-30 19:49:53 -0400

    initial files and structure created

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let library = [];
+
+function Book(title, author, pages, readStatus, id){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.id = id;
+}
+
+function addBookToLibrary(){
+    
+}
