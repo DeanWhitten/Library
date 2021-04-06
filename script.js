45006dae82581ce9170e792b32b419742f3e807e
Dean Whitten <dean.f.whitten@gmail.com>
2021-04-06 03:35:17 -0400

    update to button/ popup functions. temp styling.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+function showAddNewBookMsg(){
+        document.getElementById("addBookMsg").style.opacity = 1;
+        document.getElementById("addBookMsg").style.marginLeft = 0;
+    }
+function hideAddNewBook(){
+    document.getElementById("addBookMsg").style.opacity = 0;
+    document.getElementById("addBookMsg").style.marginLeft = "100%";
+}
+
+var x = document.getElementById("addBook_btn");
+x.addEventListener("click", showAddNewBookMsg);
+
+var y = document.getElementById("exit_btn");
+y.addEventListener("click", hideAddNewBook);
+
+var z = document.getElementById("addBook_Btn");  
+z.addEventListener("click", addBookToLibrary);
+
+
 
 let library = [];
 
-function Book(title, author, pages, readStatus, id){
+function book (title, author, pages, readStatus, id){
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -10,5 +29,10 @@ function Book(title, author, pages, readStatus, id){
 }
 
 function addBookToLibrary(){
-    
+    document.getElementById("addBookMsg").style.backgroundColor = "orange";
 }
+
+
+
+
+
