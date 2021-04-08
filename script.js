f6c330a46945637b403e194b572d67232dcc2c36
Dean Whitten <dean.f.whitten@gmail.com>
2021-04-08 01:34:24 -0400

    UI added to and modified,inital funtion instructions for handing inputs added. menu handiling/ui events added.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function showAddNewBookMsg(){
         document.getElementById("addBookMsg").style.opacity = 1;
         document.getElementById("addBookMsg").style.marginLeft = 0;
+        document.getElementById("body").style.backgroundColor = "#908e9f";
+        document.getElementById("addBook_menu").style.cssText ="border-radius: 3px;background: linear-gradient(145deg, #8b8999, #a5a3b6);box-shadow:  3px 3px 6px #8f8d9e,-3px -3px 6px #a5a3b6;";
     }
 function hideAddNewBook(){
     document.getElementById("addBookMsg").style.opacity = 0;
     document.getElementById("addBookMsg").style.marginLeft = "100%";
+    document.getElementById("body").style.backgroundColor = "#ffffff";
+    document.getElementById("addBook_menu").style.cssText ="border-radius: 3px;background: linear-gradient(145deg, #ffffff, #e6e6e6);box-shadow:  3px 3px 6px #ededed,-3px -3px 6px #ffffff;";
 }
 
-var x = document.getElementById("addBook_btn");
+var x = document.getElementById("addBook_menu");
 x.addEventListener("click", showAddNewBookMsg);
+
 
 var y = document.getElementById("exit_btn");
 y.addEventListener("click", hideAddNewBook);
@@ -29,7 +34,21 @@ function book (title, author, pages, readStatus, id){
 }
 
 function addBookToLibrary(){
-    document.getElementById("addBookMsg").style.backgroundColor = "orange";
+     var inputTitle, inputAuthor, inputpages, inputReadStatus;
+
+
+     inputTitle = document.getElementById("title").value;
+     inputAuthor = document.getElementById("author").value;
+     inputpages = document.getElementById("pages").value;
+     inputReadStatus = document.getElementById("readStatus").value;
+
+     console.log( inputTitle, inputAuthor, inputpages, inputReadStatus);
+
+     inputTitle = document.getElementById("title").value ="";
+     inputAuthor = document.getElementById("author").value ="";
+     inputpages = document.getElementById("pages").value ="";
+     inputReadStatus = document.getElementById("readStatus").value ="";
+
 }
 
 
